d3d0ef984fbbd656af1a5a68b084089010a3e1bd
Raquel De Biase <raqueldebiase@gmail.com>
2024-09-19 15:30:29 +0100

    add div plus onclick expo1

--- src/app/components/molecules/ExpoCard.tsx
+++ src/app/components/molecules/ExpoCard.tsx
@@ -1,52 +1,35 @@
-'use client';
+// src/app/components/molecules/ExpoCard.tsx
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Image from 'next/image';
-import { Artwork } from '@/app/hooks/useArtworkById'; // Importe a interface
+import { Artwork } from '@/app/hooks/useArtworkById';
 
 interface ExpoCardProps {
   artwork: Artwork;
+  onClick: () => void; // Função chamada quando o cartão é clicado
 }
 
-const ExpoCard: React.FC<ExpoCardProps> = ({ artwork }) => {
-  const [imageSize, setImageSize] = useState({ width: 0, height: 0 });
-
-  useEffect(() => {
-    // Função para carregar a imagem e obter suas dimensões
-    const loadImage = () => {
-      const img = new window.Image();
-      img.src = artwork.imageUrl;
-      img.onload = () => {
-        setImageSize({ width: img.width, height: img.height });
-      };
-    };
-
-    loadImage();
-  }, [artwork.imageUrl]);
-
+const ExpoCard: React.FC<ExpoCardProps> = ({ artwork, onClick }) => {
   return (
-    <div key={artwork.id} className="px-4">
-      {imageSize.width > 0 && imageSize.height > 0 && (
-        <Image
-          src={artwork.imageUrl}
-          alt={artwork.title}
-          width={imageSize.width}
-          height={imageSize.height}
-          className="rounded-sm mb-4"
-          quality={100} // Alta qualidade para manter a nitidez
-          layout="intrinsic" // Mantém a proporção original da imagem
-        />
-      )}
-      <div className='mb-24'>
-        <h3 className="text-xl mb-6">{artwork.artistDisplayName || 'Unknown'}</h3>
-        <div className=''>
-          <p className="text-sm text-gray-500">{artwork.title}</p>
-          <p className="text-sm text-gray-500">{artwork.objectDate || 'N/A'}</p>
-          <p className="text-sm text-gray-500">{artwork.medium || 'N/A'}</p>
-          <p className="text-sm text-gray-500">Dimensions: {artwork.dimensions || 'N/A'}</p>
-          <p className="text-sm text-gray-500">{artwork.repository || 'N/A'}</p>
-        </div>
-      </div> 
+    <div 
+      className="p-8 mb-16 cursor-pointer" 
+      onClick={onClick} // Chama a função onClick ao clicar
+    >
+      <Image
+        src={artwork.imageUrl}
+        alt={artwork.title}
+        width={600}
+        height={400}
+        layout="intrinsic"
+        quality={100}
+        className="rounded-sm mb-4"
+      />
+      <h3 className="text-xl mb-6">{artwork.artistDisplayName || 'Unknown'}</h3>
+      <p className="text-sm text-gray-500">{artwork.title}</p>
+      <p className="text-sm text-gray-500">{artwork.objectDate || 'N/A'}</p>
+      <p className="text-sm text-gray-500">{artwork.medium || 'N/A'}</p>
+      <p className="text-sm text-gray-500">Dimensions: {artwork.dimensions || 'N/A'}</p>
+      <p className="text-sm text-gray-500">{artwork.repository || 'N/A'}</p>
     </div>
   );
 };
--- src/app/components/organisms/AnnunciationExpo.tsx
+++ src/app/components/organisms/AnnunciationExpo.tsx
@@ -1,18 +1,23 @@
+// src/app/pages/AnnunciationExpo.tsx
+
 'use client';
 
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { useArtworkById } from '@/app/hooks/useArtworkById';
 import ExpoCard from '@/app/components/molecules/ExpoCard';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
-
+import { Artwork } from '@/app/hooks/useArtworkById';
+import Image from 'next/image';
 
 const AnnunciationExpo: React.FC = () => {
+  const [selectedArtwork, setSelectedArtwork] = useState<Artwork | null>(null);
   const artworkIds = [459055, 437490, 347404, 436096, 435899, 441233, 436791, 466182, 468106, 466256, 471086, 459062, 436476, 364797, 336173, 364735, 466685, 464404];
-
+  
   const { data, loading, error } = useArtworkById(artworkIds);
+
+  const detailsRef = useRef<HTMLDivElement | null>(null);
 
   const settings = {
     dots: true,
@@ -22,18 +27,62 @@ const AnnunciationExpo: React.FC = () => {
     slidesToScroll: 1,
   };
 
+  const handleCardClick = (art: Artwork) => {
+    setSelectedArtwork(art);
+    if (detailsRef.current) {
+      detailsRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+
   return (
-    <div className=" mx-auto p-8 mb-24">
-      <h2 className="text-4xl font-bold mb-8">Artwork Gallery</h2>
+    <div className="mx-auto mb-24">
+      <h2 className="text-4xl p-8 font-bold mb-8">Artwork Gallery</h2>
       {loading && <p>Carregando obras...</p>}
       {error && <p className="text-red-500">{error}</p>}
       {!loading && !error && data.length === 0 && <p>Nenhuma obra encontrada.</p>}
       {!loading && !error && data.length > 0 && (
-        <Slider {...settings}>
-          {data.map((art) => (
-            <ExpoCard key={art.id} artwork={art} />
-          ))}
-        </Slider>
+        <>
+          <Slider {...settings}>
+            {data.map((art) => (
+              <ExpoCard 
+                key={art.id} 
+                artwork={art} 
+                onClick={() => handleCardClick(art)} // Passa a obra para o estado e rola até a div
+              />
+            ))}
+          </Slider>
+          <div ref={detailsRef} className="mt-48 bg-gray-50 ">
+            {selectedArtwork && (
+              <section className='p-8'>
+                <div className='flex'>
+                  <div className="tec-card w-1/2">
+                    <h3 className="text-2xl my-4">{selectedArtwork.title}</h3>
+                    <p className="text-lg mb-1"><strong>Artist:</strong> {selectedArtwork.artistDisplayName || 'Unknown'}</p>
+                    <p className="text-md mb-1"><strong>Year:</strong> {selectedArtwork.objectDate || 'N/A'}</p>
+                    <p className="text-md mb-1"><strong>Medium:</strong> {selectedArtwork.medium || 'N/A'}</p>
+                    <p className="text-md mb-1"><strong>Dimensions:</strong> {selectedArtwork.dimensions || 'N/A'}</p>
+                    <p className="text-lg mb-2">{selectedArtwork.repository || 'N/A'}</p>
+                  </div>
+                  <div className="more-info w-1/2">
+                    <div className="mt-4">
+                      <Image
+                        src={selectedArtwork.imageUrl}
+                        alt={selectedArtwork.title}
+                        width={600}
+                        height={400}
+                        layout="intrinsic"
+                        quality={100}
+                      />
+                    </div>
+                  </div>
+                  
+                </div>
+                
+
+              </section>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
