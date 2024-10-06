c41358a465a092a772ca863489ecdc79cd3ff972
Raquel De Biase <raqueldebiase@gmail.com>
2024-10-06 10:13:44 +0100

    add X to menu mobile

--- src/app/components/molecules/Menu.tsx
+++ src/app/components/molecules/Menu.tsx
@@ -1,5 +1,3 @@
-// components/Menu.tsx
-
 import React, { useState } from 'react';
 import { usePathname } from 'next/navigation';
 
@@ -22,11 +20,30 @@ export const Menu: React.FC = () => {
 
   return (
     <div className="text-gray p-4 cursor-pointer" onClick={toggleMenu}>
-      <div className="flex flex-col items-end justify-end space-y-1">
-        <span className="w-8 h-1 bg-white"></span>
-        <span className="w-12 h-1 bg-white"></span>
-        <span className="w-6 h-1 bg-white"></span>
+      <div className="flex flex-col items-end justify-end space-y-1 relative w-12 h-8">
+        {/* Span 1 */}
+        <span
+          className={`w-8 h-1 bg-white transition-transform duration-300 ease-in-out transform ${
+            isOpen ? 'rotate-45 translate-y-1' : ''
+          }`}
+        ></span>
+
+        {/* Span 2 - o span do meio desaparece ao abrir */}
+        <span
+          className={`w-12 h-1 bg-white transition-opacity duration-300 ease-in-out ${
+            isOpen ? '-rotate-45 -translate-y-1 w-8' : ''
+          }`}
+        ></span>
+
+        {/* Span 3 */}
+        <span
+          className={`w-6 h-1 bg-white transition-transform duration-300 ease-in-out transform ${
+            isOpen ? 'opacity-0' : ''
+          }`}
+        ></span>
       </div>
+
+      {/* Menu navegação */}
       {isOpen && (
         <nav className="absolute right-0 text-white text-end px-12 py-8">
           <ul>
@@ -38,7 +55,7 @@ export const Menu: React.FC = () => {
             )}
             <li className="relative group mb-2">
               <a href="#gallery" onClick={() => handleScrollToSection('gallery')}>GALLERY</a>
-              <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-white transition-all duration-400 group-hover:w-full"></span>
+              <span className="absolute rigth-0 bottom-0 w-0 h-[2px] bg-white transition-all duration-400 group-hover:w-full"></span>
             </li>
             <li className="relative group mb-2">
               <a href="#temporary-exhibitions" onClick={() => handleScrollToSection('temporary-exhibitions')}>EXPLORE</a>
